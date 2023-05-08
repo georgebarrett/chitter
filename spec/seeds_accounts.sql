fc3ce6786a27c11332b6a411ea7bb6400ece64f0
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-08 23:12:43 +0100

    seeds for 'accounts' table created

---
+++ spec/seeds_accounts.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE accounts RESTART IDENTITY;
+
+INSERT INTO accounts (user_name, email, password) VALUES ('George', 'george@gmail.com', 'lol');
+INSERT INTO accounts (user_name, email, password) VALUES ('Aphra', 'aphra@gmail.com', 'tbh');
