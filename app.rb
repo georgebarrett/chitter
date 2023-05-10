a7d05e51c917db1ad3399241abc782f0c523b738
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-10 10:50:15 +0100

    sinatra ready for methods

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
-require 'sinatra/base'
-require 'sinatra/reloader'
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/database_connection'
+require_relative 'lib/account_repository'
+require_relative 'lib/post_repository'
+
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
-  # This allows the app code to refresh
-  # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/account_repository'
+    also_reload 'lib/post_repository'
   end
+
+    
+
+
+
 end
+
